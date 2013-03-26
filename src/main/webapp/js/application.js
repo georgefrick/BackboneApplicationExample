daa70cac00f8d0db6a5d473c4ed25631309f83a1
George Frick <george.frick@gmail.com>
2013-03-25 19:02:16 -0500

    add the calculator, close all tabs, and fix up other features. Still needs todo added.

--- src/main/webapp/js/application.js
+++ src/main/webapp/js/application.js
@@ -15,11 +15,18 @@
         }
     }
 
-    // Add a loader to handlebars.
+    // Extend Handlebars to load [and cache] a template from within the DOM.
     if (Handlebars && !Handlebars.loadTemplate) {
-        Handlebars.loadTemplate = function (name) {
-            return Handlebars.compile($('#' + name + '-template').html());
+        var loadTemplate = function (name) {
+            if (this[name]) {
+                return this[name];
+            }
+            var temp = $("#" + name + "-template").html();
+            var compiled = Handlebars.compile(temp);
+            this[name] = compiled;
+            return compiled;
         };
+        Handlebars.loadTemplate = loadTemplate;
     }
 
     /* Everything into its own scope. */
@@ -34,8 +41,7 @@
         initialize:function (options) {
             this.el = options.el;
             this.tabTemplate = Handlebars.loadTemplate("tab");
-            this.tabContentTemplate = Handlebars.loadTemplate("tabContent");
-            this.calculatorIndex = 0;
+            this.tabContentTemplate = Handlebars.loadTemplate("tabContent");            
         },
         routes:{
             "":"showHome",
@@ -52,33 +58,87 @@
         },
         newCalculator:function () {
             // For the calculator, we allow multiple tabs...
-            var index = "calculator" + this.calculatorIndex;
-            var calc = new Calculator.CalculatorView();
-            this.addTab("Calculator " + this.calculatorIndex, calc.render().el, index );
-            this.calculatorIndex++;
+        	 var tabId = "calculator-" + Math.guid();
+             var tabName = "Calculator";
+             var calc = new Calculator.CalculatorView();
+             var uniqueId = this.addTab({
+                 'id' : tabId,
+                 'name' : tabName
+             });
+             var tabContent = $("#" + uniqueId);
+             tabContent.empty();
+             tabContent.append(calc.render().el);                       
             Backbone.history.navigate("#");
         },
         newBookList:function () {
-            var shelf = new BookDatabase.Shelf();
-            this.addTab("Bookshelf", shelf.render().el );
-            Backbone.history.navigate("#");
-        },
-        addTab:function (name, content, idIn) {
-            var id = Math.guid();
-            if( idIn ) {
-                id = idIn;
+            if (!this.openBookShelf
+                || ($("#" + this.openBookShelf).length == 0)) {
+                var tabId = "book-shelf" + Math.guid();
+                var tabName = "Book Shelf";
+                var shelf = new BookDatabase.Shelf();
+                this.openBookShelf = this.addTab({
+                    'id' : tabId,
+                    'name' : tabName
+                });
+                var tabContent = $("#" + this.openBookShelf);
+                tabContent.empty();
+                tabContent.append(shelf.render().el);
+            } else if (this.openBookShelf) {
+                this.selectTabById(this.openBookShelf);
             }
-            var newTab = {
-                id:id,
-                label: name
+            Backbone.history.navigate(""); // for now.
+        },
+        addTab : function(tabInfo) {
+            var current = {
+                // Defaults
+                'name' : "New Tab",
+                'content' : "",
+                'show' : true,
+                'id' : Math.guid()
             };
-            var li = this.tabTemplate(newTab);
-            var fullContent = this.tabContentTemplate(newTab);
-            fullContent = $(fullContent).append(content);
+            var attr, val;
+            var attrs = {};
 
-            this.el.find(".ui-tabs-nav").append(li);
-            this.el.append(fullContent);
+            // Check if just the name was passed in, or an object full of properties.
+            if (typeof tabInfo === 'object') {
+                attrs = tabInfo;
+            } else {
+                attrs.name = tabInfo;
+            }
+
+            // Look for passed in options that override defaults and set them.
+            for (attr in attrs) {
+                val = attrs[attr];
+                if (current[attr] !== undefined && !_.isEqual(current[attr], val)) {
+                    current[attr] = val;
+                }
+            }
+
+            // Construct the new tab, consisting of the tab and the tab content.
+            this.el.find(".ui-tabs-nav:first").append(this.tabTemplate(current));
+            this.el.append(this.tabContentTemplate(current));
             this.el.tabs("refresh");
+
+            // If the user opted not to show the tab, it is opened in the background.
+            if (current.show) {
+                this.selectTabById(current.id);
+            }
+            return current.id;
+        },
+        selectTabById : function(tabId) {
+            var index = $('#tabs a[href="' + tabId + '"]').parent().index();
+            return $("#" + tabId).length && $("#tabs").tabs( "option", "active",index);
+        },
+        closeAllTabs:function () {
+            var tabCount = $('#tabs >ul >li').size();
+            while (tabCount > 1) {
+                var tab = $( "#tabs" ).find( ".ui-tabs-nav li:eq(1)" ).remove();
+                var panelId = tab.attr( "aria-controls" );
+                $( "#" + panelId ).remove();
+                $( "tabs" ).tabs( "refresh" );
+                tabCount--;
+            }
+            Backbone.history.navigate(""); // for now.
         }
     });
 
@@ -86,6 +146,7 @@
         container = $(container);
         var router = new Application.Router({el:container});
         Backbone.history.start();
+        return router;
     };
 
 })
